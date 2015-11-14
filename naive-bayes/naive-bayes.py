f489dcf4826e04dcd6726c6def435fd242115607
Remy Oukaour <remy.oukaour@gmail.com>
2015-11-13 23:50:09 -0500

    Output Excel-quality confusion matrix with statistics

--- naive-bayes/naive-bayes.py
+++ naive-bayes/naive-bayes.py
@@ -1,4 +1,6 @@
-from __future__ import print_function
+#!/usr/bin/python
+
+from __future__ import print_function, division
 
 from collections import namedtuple, defaultdict
 from math import log, log1p
@@ -14,12 +16,23 @@ class NaiveBayesClassifier(object):
 		self.label_count = defaultdict(lambda: 0)
 		self.feature_count = defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: 0)))
 
+	def labels(self):
+		return sorted(self.label_count.keys())
+
 	def train(self, data):
 		for datum in data:
 			self.corpus_count += 1
 			self.label_count[datum.label] += 1
 			for i, v in enumerate(datum.features):
 				self.feature_count[datum.label][i][v] += 1
+
+	def classify(self, data):
+		for datum in data:
+			yield Classification(self.classify_features(datum.features), datum.label)
+
+	def classify_features(self, features):
+		# Maximum a posteriori estimation: arg max(P(features|label) for each label)
+		return max(self.labels(), key=lambda label: self.log_posterior(features, label))
 
 	def log_prior(self, label):
 		# P(label) = # label / # all labels in training set
@@ -28,7 +41,7 @@ class NaiveBayesClassifier(object):
 
 	def log_posterior(self, features, label):
 		# P(features|label) = P(label) * P(label|features) / P(label)
-		# log P(features|label) = log P(label) + log P(label|features) / log P(label)
+		# log P(features|label) = log P(label) + log P(label|features) - log P(label)
 		# (we leave out the evidence value, P(label), since it is constant)
 		return self.log_prior(label) + self.log_likelihood(label, features)
 
@@ -40,18 +53,9 @@ class NaiveBayesClassifier(object):
 	def log_feature_likelihood(self, label, i, v):
 		# P(label|feature value) = # value / # all values for feature
 		# log P(label|feature value) = log # value - log # all values for feature
-		# (we use Laplace smoothing to avoid log(0) = -infinity)
+		# (we use Laplace smoothing, adding 1 to the numerator and denominator,
+		# to handle zero counts (particularly since log 0 = -infinity))
 		return log1p(self.feature_count[label][i][v]) - log1p(self.label_count[label])
-
-	def labels(self):
-		return self.label_count.keys()
-
-	def classify(self, data):
-		for datum in data:
-			yield Classification(self.classify_features(datum.features), datum.label)
-
-	def classify_features(self, features):
-		return max(self.labels(), key=lambda label: self.log_posterior(features, label))
 
 def digit_data(label_filename, feature_filename):
 	with open(label_filename, 'r') as label_file, open(feature_filename, 'r') as feature_file:
@@ -67,20 +71,52 @@ def digit_data(label_filename, feature_filename):
 			yield Datum(label, features)
 
 def main():
+	# Train naive Bayes classifier on training data
 	nbc = NaiveBayesClassifier()
 	training_data = digit_data('traininglabels.txt', 'trainingimages.txt')
 	nbc.train(training_data)
-	test_data = digit_data('testlabels.txt', 'testimages.txt')
+	assert len(nbc.labels()) == 10
 
-	confusion_matrix = defaultdict(lambda: defaultdict(lambda: 0))
+	# Classify test data, output classifications, and build a confusion matrix
+	test_data = digit_data('testlabels.txt', 'testimages.txt')
+	confusion_matrix = [[0] * 10 for _ in range(10)]
 	with open('predictedlabels.txt', 'w') as prediction_file:
 		for result in nbc.classify(test_data):
 			confusion_matrix[result.predicted][result.true] += 1
 			prediction_file.write('%d\n' % result.predicted)
 
-	for predicted_label in sorted(nbc.labels()):
-		print(*(confusion_matrix[predicted_label][actual_label]
-			for actual_label in sorted(nbc.labels())), sep='\t')
+	# Calculate row totals
+	for row in confusion_matrix:
+		row.append(sum(row))
+	# Calculate column totals
+	confusion_matrix.append([sum(zip(*confusion_matrix)[i]) for i in range(len(confusion_matrix[0]))])
+	# Calculate precision scores
+	for i, row in enumerate(confusion_matrix):
+		row.append(row[i] / row[-1])
+	# Calculate sensitivity scores
+	confusion_matrix.append([confusion_matrix[i][i] / confusion_matrix[-1][i]
+		for i in range(len(confusion_matrix[0]) - 1)] + [''])
+	# Add row labels
+	for i, label in enumerate(nbc.labels()):
+		confusion_matrix[i].append(label)
+	# Add column labels
+	confusion_matrix.append(list(nbc.labels()))
+	# Add row and column total labels
+	confusion_matrix[-3].append('total')
+	confusion_matrix[-1].append('total')
+	# Add precision and sensitivity labels
+	confusion_matrix[-1].append('precision')
+	confusion_matrix[-2].append('sensitivity')
+	# Add overall axis label
+	confusion_matrix[-1].append('pred\\true')
+	# Move labels to top and left, not bottom and right
+	for row in confusion_matrix:
+		row.insert(0, row.pop())
+	confusion_matrix.insert(0, confusion_matrix.pop())
+
+	# Output confusion matrix
+	for row in confusion_matrix:
+		print(*row, sep='\t')
 
 if __name__ == '__main__':
 	main()
